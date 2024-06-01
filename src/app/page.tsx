7645fa6e4c7cc6db1811f182e38da883da1bd98e
Petter Sæther Moen <pettersmoen@gmail.com>
2024-06-01 22:07:19 +0200

    improve front page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,12 @@ export default async function Home() {
   } = await octokit.rest.users.getAuthenticated();
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="mx-auto flex min-h-screen w-full max-w-7xl flex-col items-center gap-4 p-4">
       <h1 className="font-bold text-4xl">Hi, {login}!</h1>
-      <RepoView owner="petter" repo="pmoen.me" />
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 sm:grid-cols-2">
+        <RepoView owner="petter" repo="pmoen.me" />
+        <RepoView owner="domstolene" repo="designsystem" />
+      </div>
     </main>
   );
 }
