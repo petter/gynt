befa23825c080b2b4f063192dbf20224bfb442cb
Petter Sæther Moen <pettersmoen@gmail.com>
2024-06-02 14:21:15 +0200

    render html in markdown

--- src/components/markdown.tsx
+++ src/components/markdown.tsx
@@ -1,9 +1,19 @@
 import ReactMarkdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
 import remarkGfm from "remark-gfm";
 
 interface Props {
   children: string | null;
 }
 export function Markdown({ children }: Props) {
-  return <ReactMarkdown remarkPlugins={[remarkGfm]}>{children}</ReactMarkdown>;
+  console.log(children);
+  return (
+    <ReactMarkdown
+      remarkPlugins={[remarkGfm]}
+      rehypePlugins={[rehypeRaw]}
+      disallowedElements={["script"]}
+    >
+      {children}
+    </ReactMarkdown>
+  );
 }
