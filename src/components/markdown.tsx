96001bb694a67c506266a85e86b1ee5ee26faf41
Petter Sæther Moen <pettersmoen@gmail.com>
2024-06-03 20:07:19 +0200

    remove console.logging in markdown component

--- src/components/markdown.tsx
+++ src/components/markdown.tsx
@@ -7,7 +7,6 @@ interface Props {
   children: string | null;
 }
 export function Markdown({ children }: Props) {
-  console.log(children);
   return (
     <ReactMarkdown
       remarkPlugins={[remarkGfm]}
