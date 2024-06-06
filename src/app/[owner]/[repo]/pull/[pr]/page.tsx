258c5c9027429f4ce68ab8dcacbffd3f258e985f
Petter Sæther Moen <pettersmoen@gmail.com>
2024-06-06 21:02:47 +0200

    add lgtm button

--- src/app/[owner]/[repo]/pull/[pr]/page.tsx
+++ src/app/[owner]/[repo]/pull/[pr]/page.tsx
@@ -3,6 +3,7 @@ import { PullRequestBadge } from "@/components/pull-request/badge";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import { prStatus } from "@/lib/pull-request";
+import { cn } from "@/lib/utils";
 import { getOctokit } from "@/server/octokit";
 import { notFound } from "next/navigation";
 
@@ -11,6 +12,17 @@ interface Props {
 }
 
 export default async function PullPage({ params }: Props) {
+  async function lgtm() {
+    "use server";
+    const octokit = await getOctokit();
+    await octokit.rest.pulls.createReview({
+      owner: params.owner,
+      repo: params.repo,
+      pull_number: +params.pr,
+      event: "APPROVE",
+      body: 'LGTM! 🚀\n\n*Looks good with <a href="https://gynt.app/">gynt</a>* 😎',
+    });
+  }
   async function addComment(formData: FormData) {
     "use server";
     const octokit = await getOctokit();
@@ -47,11 +59,15 @@ export default async function PullPage({ params }: Props) {
     repo: params.repo,
     pull_number: prNum,
   };
-  const [{ data: pr }, { data: files }, { data: reviews }] = await Promise.all([
-    octokit.rest.pulls.get(commonParams).catch(() => notFound()),
-    octokit.rest.pulls.listFiles(commonParams).catch(() => notFound()),
-    octokit.rest.pulls.listReviews(commonParams).catch(() => notFound()),
-  ]);
+  const [{ data: user }, { data: pr }, { data: files }, { data: reviews }] =
+    await Promise.all([
+      octokit.rest.users.getAuthenticated(),
+      octokit.rest.pulls.get(commonParams).catch(() => notFound()),
+      octokit.rest.pulls.listFiles(commonParams).catch(() => notFound()),
+      octokit.rest.pulls.listReviews(commonParams).catch(() => notFound()),
+    ]);
+
+  const isMyPr = pr.user.login === user.login;
 
   return (
     <div className="flex flex-col gap-4">
@@ -79,6 +95,19 @@ export default async function PullPage({ params }: Props) {
       <div className="h-96 overflow-y-scroll rounded-xl border p-4 shadow">
         <Markdown>{pr.body}</Markdown>
       </div>
+      <form action={lgtm}>
+        <button
+          type="submit"
+          disabled={isMyPr}
+          className={cn(
+            "rounded-lg bg-green-500 px-4 py-2 font-bold text-white",
+            !isMyPr && "hover:bg-green-600",
+            isMyPr && "cursor-not-allowed opacity-50",
+          )}
+        >
+          LGTM! :)
+        </button>
+      </form>
       <div>
         <h2 className="text-2xl">Files changed</h2>
         {files.map((file) => (
